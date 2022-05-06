fadca594c02d02d8abb0a74323806505e346f82a
jbsselim <selim.jean-baptiste-simonne@epitech.eu>
2022-05-06 18:29:09 +0200

    added Components and assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import { About, Footer, Skills, Header, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
+
+import "./App.scss";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 };
