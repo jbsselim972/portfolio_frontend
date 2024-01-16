315da9ca177c62d715b0c7cf86f9af4c412d809a
jbsselim <jbsselim@gmail.com>
2024-01-16 10:54:27 -0400

    added loading skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,22 @@ import { Analytics } from '@vercel/analytics/react'
 import { Suspense } from 'react'
 
 import Navbar from './components/Navbar/Navbar'
+import { Skeleton } from './components/UI/skeleton'
 import { About, Footer, Header, Quote, Skills, Work } from './container'
+
+const LoadingApp = () => {
+    return (
+        <div className="flex flex-col p-0 gap-y-2 h-full">
+            <Skeleton className="flex h-[100px] bg-gray-400  rounded-none" />
+            <Skeleton className="flex h-[640px] bg-gray-400 rounded-none" />
+        </div>
+    )
+}
 
 const App = () => {
     const year = new Date().getFullYear()
     return (
-        <Suspense fallback="loading">
+        <Suspense fallback={<LoadingApp />}>
             <div className="app">
                 <Navbar />
                 <Header />
