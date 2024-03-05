0b417b159b16037cf49a507f13be60187d57863e
jbsselim <jbsselim@gmail.com>
2024-03-05 14:26:17 -0400

    added zod & react hook form to validate contact form

--- src/container/Footer/Footer.tsx
+++ src/container/Footer/Footer.tsx
@@ -1,100 +1,105 @@
 import '@/styles/Footer.css'
 
-import { ChangeEvent, FormEvent, useRef, useState } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { AiFillCheckCircle } from 'react-icons/ai'
+import { z } from 'zod'
 
-// import { client } from "../../client";
 import { images } from '@/constants'
 import { sendEmail } from '@/utils/email'
 
+const formSchema = z.object({
+    name: z.string().min(1).optional(),
+    email: z.string().email().min(1, 'Email is required.'),
+    message: z.string().optional(),
+})
+
+export type FormSchema = z.infer<typeof formSchema>
+
 const Footer = () => {
     const { t } = useTranslation()
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: '',
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormSchema>({
+        resolver: zodResolver(formSchema),
     })
+
     const [formSubmitted, setFormSubmitted] = useState(false)
     const [loading, setLoading] = useState(false)
-    const form = useRef<HTMLFormElement>(null)
 
-    const { name, email, message } = formData
-
-    const handleChangeInput = (
-        e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
-    ) => {
-        const { name, value } = e.target
-
-        setFormData({ ...formData, [name]: value })
-    }
-
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault()
-        setLoading(true)
-
-        await sendEmail(form)
-        setLoading(false)
-        setFormSubmitted(true)
-        // const contact = {
-        //   _type: "contact",
-        //   name,
-        //   email,
-        //   message,
-        // };
-
-        // client.create(contact).then(() => {
-        //   setLoading(false);
-        //   setFormSubmitted(true);
-        // });
+    const onSubmit: SubmitHandler<FormSchema> = async (data) => {
+        try {
+            await sendEmail(data)
+            setFormSubmitted(true)
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     return (
         <>
-            <h2 className="text-[1.5rem] sm:text-[2rem] font-[800]">
-                {t('contactTitle')} &#128521;
-            </h2>
-
-            <div className="app__footer-cards">
-                <div className="app__footer-card">
-                    <img src={images.email} alt="email" />
-                    <a href="mailto:jbsselim@gmail.com">jbsselim@gmail.com</a>
-                </div>
-            </div>
-
             {!formSubmitted ? (
-                <form ref={form} className="app__footer-form app__flex" onSubmit={handleSubmit}>
-                    <div className="app__flex">
-                        <input
-                            className="p-text"
-                            type="text"
-                            placeholder={t('namePlaceholder')}
-                            name="name"
-                            value={name}
-                            onChange={handleChangeInput}
-                        />
+                <>
+                    <h2 className="text-[1.5rem] sm:text-[2rem] font-[800]">
+                        {t('contactTitle')} &#128521;
+                    </h2>
+
+                    <div className="app__footer-cards">
+                        <div className="app__footer-card">
+                            <img src={images.email} alt="email" />
+                            <a href="mailto:jbsselim@gmail.com">jbsselim@gmail.com</a>
+                        </div>
                     </div>
-                    <div>
-                        <input
-                            className="p-text"
-                            type="text"
-                            placeholder={t('emailPlaceholder')}
-                            name="email"
-                            value={email}
-                            onChange={handleChangeInput}
-                        />
-                    </div>
-                    <div>
-                        <textarea
-                            className="p-text"
-                            placeholder={t('textareaPlaceholder')}
-                            name="message"
-                            value={message}
-                            onChange={handleChangeInput}
-                        />
-                    </div>
-                    <button type="submit">{loading ? t('sendTextLoad') : t('sendText')}</button>
-                </form>
+
+                    <form className="app__footer-form app__flex" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="app__flex">
+                            <input
+                                className="p-text"
+                                type="text"
+                                placeholder={t('namePlaceholder')}
+                                {...register('name')}
+                            />
+                        </div>
+                        {errors.name && (
+                            <p className="text-xs italic text-red-500 mt-2">
+                                {' '}
+                                {errors.name?.message}
+                            </p>
+                        )}
+                        <div>
+                            <input
+                                className="p-text"
+                                type="text"
+                                placeholder={t('emailPlaceholder')}
+                                {...register('email')}
+                            />
+                        </div>
+                        {errors.email && (
+                            <p className="flex justify-start text-xs italic text-red-500 mt-2">
+                                {' '}
+                                {errors.email?.message}
+                            </p>
+                        )}
+                        <div>
+                            <textarea
+                                className="p-text"
+                                placeholder={t('textareaPlaceholder')}
+                                {...register('message')}
+                            />
+                        </div>
+                        {errors.message && (
+                            <p className="text-xs italic text-red-500 mt-2">
+                                {' '}
+                                {errors.message?.message}
+                            </p>
+                        )}
+                        <button type="submit">{loading ? t('sendTextLoad') : t('sendText')}</button>
+                    </form>
+                </>
             ) : (
                 <div className="app__footer-form-success">
                     <AiFillCheckCircle />
--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,11 +1,11 @@
 import emailjs from '@emailjs/browser'
-import { RefObject } from 'react'
 
-export const sendEmail = (form: RefObject<HTMLFormElement>) => {
+import { FormSchema } from '@/container/Footer/Footer'
+
+export const sendEmail = async (data: FormSchema) => {
     const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
     const userId = import.meta.env.VITE_USER_ID
     const templateId = import.meta.env.VITE_TEMPLATE_ID
 
-    const formRef = form.current ? form.current : ''
-    return emailjs.sendForm(userId, templateId, formRef, publicKey)
+    return await emailjs.send(userId, templateId, data, publicKey)
 }
